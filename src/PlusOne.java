322e1a46efebe91199171dd43c917c64fe4e2eb3
Fu4ng <903838834@qq.com>
2018-04-25 12:21:51 +0800

    7 加一

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -1,30 +1,38 @@
 public class PlusOne {
     public static int[] plusOne(int[] digits) {
-        long  temp = 0;
-        int [] result;
+        int[] result;
+        int len = digits.length;
+        int f = 0;
+        digits[digits.length-1]=(digits[digits.length-1]+1)%10;
+        if(digits[digits.length-1] == 0) f=1;
+        while(len-- > 0){
+            if(digits[len]==0 && len!=0&& f==1){
+                //需要进位
+                digits[len-1]=(digits[len-1]+1)%10;
+                if(digits[len-1]==0) f=1;
+                else f=0;
+            }
+        }
         if(digits[0]==0){
-            result =new int[1];
-            result[0]=1;
-            return result;
-        }
-        int len = digits.length-1;
-        while(len>=0){
-            temp+=(long)(digits[digits.length-1-len] * (long)(Math.pow(10,len)));
-            len--;
-        }
-        temp+=1;
-        //判断temp位数
-        int d = (int)(Math.log10(temp))+1;
-        result = new int[d];
-        while(d!=0){
-            result[result.length-d] = (int)( temp /(long)(Math.pow(10,(d-1))));
-            temp = temp % (long)(Math.pow(10,(d-1)));
-            d--;
+            result = new int[digits.length+1];
+            result[0] = 1;
+            int i = 1;
+            while(i<=digits.length){
+                result[i]=digits[i-1];
+                i++;
+            }
+        }else{
+            result=new int[digits.length];
+            int i= 0;
+            while (i<digits.length){
+                result[i] = digits[i];
+                i++;
+            }
         }
         return result;
     }
     public static void main(String[] srgc){
-        int[] d = {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+        int[] d = {9,9,9};
         int[] t = plusOne(d);
         for (int i:t
              ) {
